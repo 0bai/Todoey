925a0080b1b5c813c247b6aa04bf07fd7ef9aba1
Obai Alnajjar <obai@outlook.com>
2019-01-06 18:22:32 +0300

    Final clean up

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -7,58 +7,54 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemsArray = [Item]()
-    let defaults = UserDefaults.standard
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if  let items = defaults.array(forKey: "ItemsList") as? [Item] {
-            itemsArray = items
-        }
+        loadItems()
     }
-
-    //MARK: Tableview Datasource Methods
+    
+    //MARK: -  Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemsArray.count
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemsArray[indexPath.row].title
-        
         cell.accessoryType = itemsArray[indexPath.row].done ? .checkmark : .none
-        
         return cell
-
     }
-
-    //MARK: TableView Delegate Methods
+    
+    
+    //MARK: -  TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         itemsArray[indexPath.row].done = !itemsArray[indexPath.row].done
         tableView.deselectRow(at: indexPath, animated: true)
-        tableView.reloadData()
+        saveItems()
     }
     
-    //MARK: Add New Item
+    
+    //MARK: -  Add New Item
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        
+
         let action = UIAlertAction(title: "Add Item", style: .default) { (UIAlertAction) in
-            self.itemsArray.append(Item(title: textField.text!))
-          //  self.defaults.set(self.itemsArray, forKey: "ItemsList")
-            self.tableView.reloadData()
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            self.itemsArray.append(newItem)
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -68,9 +64,49 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-    
     }
     
     
+    //MARK: -  Model Manipulation Methods
+    
+    func saveItems(){
+        do {
+            try context.save()
+        }catch{
+            print("Error saving context, \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+           itemsArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
 }
 
+//MARK:- Extensions
+
+//MARK:- Search Bar Extension
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
